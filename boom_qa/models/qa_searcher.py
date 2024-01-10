b7325339f05d00c798ee75502728b817ad2f1b12
Justin Gerard Ricardo <justinericardo@gmail.com>
2024-01-10 19:55:15 +0800

    Define QAEmbedder class constructor

--- boom_qa/models/qa_searcher.py
+++ boom_qa/models/qa_searcher.py
@@ -3,7 +3,12 @@ from transformers import AutoModel, AutoTokenizer
 
 
 class QAEmbedder:
-    pass
+    def __init__(
+        self,
+        model_name="sentence-transformers/paraphrase-MiniLM-L6-v2",
+    ):
+        self.model = AutoModel.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
 class QASearcher:
