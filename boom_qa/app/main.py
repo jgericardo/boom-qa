d4ae99187a3ab8ec0225f0956300d3c1afae47d8
Justin Gerard Ricardo <justinericardo@gmail.com>
2024-01-11 02:14:09 +0800

    Add main file for FastAPI app

---
+++ boom_qa/app/main.py
@@ -0,0 +1,18 @@
+"""Runner for FastAPI app"""
+from uvicorn import Config, Server
+
+from boom_qa.app.setup import application
+
+app = application()
+
+if __name__ == "__main__":
+    server = Server(
+        Config(
+            "boom_qa.main:app",
+            host="0.0.0.0",
+            port=8000,
+            use_colors=False,
+            workers=16,
+        )
+    )
+    server.run()
