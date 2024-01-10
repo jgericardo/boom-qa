42788d8234bfd0a207f18ee1b20b4045093e18d8
Justin Gerard Ricardo <justinericardo@gmail.com>
2024-01-10 20:04:42 +0800

    Add class helper method to apply mean pooling to model output

--- boom_qa/models/qa_searcher.py
+++ boom_qa/models/qa_searcher.py
@@ -11,6 +11,35 @@ class QAEmbedder:
         self.model = AutoModel.from_pretrained(model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+    def _masked_mean_pooling(self, model_output, attention_mask):
+        """
+        Helper method that transforms the model output
+        by adding attention and applying mean pooling.
+
+        Parameters
+        ----------
+        model_output: torch.Tensor
+            Output from the QA model
+        attention_mask: torch.Tensor
+            Attention mask from the QA tokenizer.
+
+        Returns
+        -------
+        pooled_embeddings: torch.Tensor
+            Averaged embedding tensor.
+        """
+        token_embeddings = model_output[0]
+
+        input_mask_expanded = (
+            attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        )
+
+        pooled_embedding = torch.sum(
+            token_embeddings * input_mask_expanded, 1
+        ) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+        return pooled_embedding
+
 
 class QASearcher:
     pass
